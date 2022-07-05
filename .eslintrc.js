381320eb103e473dab0256b2d3cb274898e8c165
许江涛 <768219063@qq.com>
2022-07-05 09:31:59 +0800

    test(test): test

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'space-before-function-paren': 'off',
-    quotes: 'warn',
+    quotes: 'off',
     'no-tabs': 'warn',
     'vue/multi-word-component-names': 'off'
   }
